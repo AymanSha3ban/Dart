c35c350ab7548adbeff1cad86ef9abbf463af413
Ayman Shaaban <aymankhattap483@gmail.com>
2025-03-07 15:50:58 +0200

    Q1.dart

--- Tut 1/Q1.dart
+++ Tut 1/Q1.dart
@@ -1,1 +1,20 @@
+// Q1 : Chec number is even or odd ?
+import 'dart:io';
+void check(int num) {
+  if (num % 2 == 0) {
+    print("Number is even (^_^)");
+  } else {
+    print("Number is odd (*_*)");
+  }
+}
 
+void main() {
+  print("Enter a number ");
+  String? num=stdin.readLineSync();
+  if(num != null){
+    int x=int.tryParse(num)!;
+  
+  check(x);
+  }
+  
+}
